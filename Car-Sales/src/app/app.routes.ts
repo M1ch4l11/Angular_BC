9313f8d69431d1baab0958fa87a69c2966b75ba7
M1ch4l11 <lojko.michal11@gmail.com>
2024-02-11 11:59:58 +0100

    Feat: create routes

--- Car-Sales/src/app/app.routes.ts
+++ Car-Sales/src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./pages/products-page/products-page.component').then(
+        (c) => c.ProductsPageComponent
+      ),
+  },
+  {
+    path: 'tables',
+    loadComponent: () =>
+      import('./pages/table-page/table-page.component').then(
+        (c) => c.TablePageComponent
+      ),
+  },
+  {
+    path: 'users',
+    loadComponent: () =>
+      import('./pages/users-page/users-page.component').then(
+        (c) => c.UsersPageComponent
+      ),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login-pages/login-pages.component').then(
+        (c) => c.LoginPagesComponent
+      ),
+  },
+];
