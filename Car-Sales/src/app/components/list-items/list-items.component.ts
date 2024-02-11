c312e8087578654514c14e463137b379401b628c
M1ch4l11 <lojko.michal11@gmail.com>
2024-02-11 12:57:04 +0100

    Feat: Create & Implement ListComponent

---
+++ Car-Sales/src/app/components/list-items/list-items.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, ViewChild, ViewContainerRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ItemComponent } from '../item/item.component';
+import { ItemType } from 'src/app/models/item-type';
+import { SearchComponent } from '../search/search.component';
+import { FormTemplatesService } from 'src/app/services/form-templates.service';
+interface User {
+  login: string;
+  password: string;
+}
+@Component({
+  selector: 'app-list-items',
+  standalone: true,
+  imports: [CommonModule, ItemComponent, SearchComponent],
+  templateUrl: './list-items.component.html',
+  styleUrls: ['./list-items.component.scss'],
+})
+export class ListItemsComponent<T> {
+  @ViewChild('dynamicComponentContainer', { read: ViewContainerRef })
+  dynamicComponentContainer!: ViewContainerRef;
+  @Input() type!: ItemType;
+  @Input() items!: T[];
+
+  constructor(private formTemplates: FormTemplatesService) {}
+
+  onloadComponent(event: { type: string; item: any }): void {
+    this.formTemplates.loadComponent(this.dynamicComponentContainer, event);
+  }
+}
